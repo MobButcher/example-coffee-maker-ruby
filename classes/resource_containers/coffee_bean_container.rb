be561943832d3dc417194b9bfc3f11809b3f0f48
Mobby Butcher <38690257+MobButcher@users.noreply.github.com>
2022-06-07 16:26:21 +0300

    Bugfix: initialize instance variables inside initialize in CoffeeBeanContainer

--- classes/resource_containers/coffee_bean_container.rb
+++ classes/resource_containers/coffee_bean_container.rb
@@ -5,12 +5,15 @@ require_relative '../validator'
 class CoffeeBeanContainer < BaseResourceContainer
   RESOURCE = CoffeeBeanResource
 
-  # Possible states:
-  # :ground - coffee beans are ground and ready for consumption
-  # :not_ground - coffee beans require grounding before consumption
-  # Empty container assumed to be :ground.
-  @state = :ground
-  @autogrind = false
+  def initialize(min: 0, max: 100, frozen: false)
+    super(min: min, max: max, frozen: frozen)
+    # Possible states:
+    # :ground - coffee beans are ground and ready for consumption
+    # :not_ground - coffee beans require grounding before consumption
+    # Empty container assumed to be :ground.
+    @state = :ground
+    @autogrind = false
+  end
 
   def add(amount)
     super
